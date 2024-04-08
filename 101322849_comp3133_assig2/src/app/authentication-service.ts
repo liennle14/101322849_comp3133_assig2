5f30eebbc6cd90f309e1913708d5819cd10e3ce8
liennle14 <liennle14@hotmail.com>
2024-04-07 22:26:38 -0400

    first commit

--- 101322849_comp3133_assig2/src/app/authentication-service.ts
+++ 101322849_comp3133_assig2/src/app/authentication-service.ts
@@ -8,7 +8,7 @@ import { User } from './user';
   providedIn: 'root'
 })
 export class AuthenticationService {
-  private baseUrl = 'https://101322849-comp3133-assig2.vercel.app:4200';
+  private baseUrl = 'https://101322849-comp3133-assig2.vercel.apps';
 
   constructor(private http: HttpClient) { }
 
